dc2c653bd9833c1e050665a9ddabe2da9fae7ef9
Paweł Szumański <pawel.szumanski@outlook.com>
2024-04-24 22:38:08 +0200

    Update routes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi_limiter import FastAPILimiter
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.conf.config import settings
-from src.routes import auth, users
+from src.routes import auth
 import uvicorn
 from dotenv import load_dotenv
 
@@ -21,9 +21,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# app.include_router(admin.router)
 app.include_router(auth.router, prefix="/api")
-app.include_router(users.router, prefix="/api")
 
 
 async def startup_event():
