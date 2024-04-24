2dee8ceeffb8febd9c9df4338c4969f4c3b91912
Paweł Szumański <pawel.szumanski@outlook.com>
2024-04-24 13:17:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ from src.routes import auth, users
 import uvicorn
 from dotenv import load_dotenv
 
-origins = [
-    "http://localhost:8000"
-    ]
+origins = ["http://localhost:8000"]
 
 load_dotenv()
 app = FastAPI()
@@ -23,9 +21,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(admin.router)
-app.include_router(auth.router, prefix='/api')
-app.include_router(users.router, prefix='/api')
+# app.include_router(admin.router)
+app.include_router(auth.router, prefix="/api")
+app.include_router(users.router, prefix="/api")
+
 
 @app.on_event("startup")
 async def startup():
@@ -35,9 +34,15 @@ async def startup():
     This function initializes a connection to Redis and sets up rate limiting.
     """
 
-    r = await Redis(host=settings.redis_host, port=settings.redis_port, db=0, encoding="utf-8",
-                    decode_responses=True)
+    r = await Redis(
+        host=settings.redis_host,
+        port=settings.redis_port,
+        db=0,
+        encoding="utf-8",
+        decode_responses=True,
+    )
     await FastAPILimiter.init(r)
+
 
 @app.get("/")
 def read_root():
@@ -49,6 +54,7 @@ def read_root():
     """
 
     return {"message": "Welcome in PhotoShareApp!"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
