b8d32607e1e9f848aa3ca03cbbe89ea78c0c1925
Paweł Szumański <pawel.szumanski@outlook.com>
2024-04-24 22:50:23 +0200

    Add set_user_role + exceptions

--- src/repository/users.py
+++ src/repository/users.py
@@ -13,34 +13,24 @@ async def count_users(db: Session):
 async def get_user_by_email(email: str, db: Session) -> UserOut:
     """
     Retrieve a user from the database by their email address.
-
     Args:
         email (str): The email address of the user to retrieve.
         db (Session): The database session.
-
     Returns:
         UserOut: The user retrieved from the database.
     """
     user = db.query(User).filter(User.email == email).first()
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with email:{email} not found",
-        )
     return user
 
 
 async def create_user(body: UserIn, db: Session) -> User:
     """
-     Create a new user in the database.
-
+    Create a new user in the database.
     Args:
         body (UserIn): The input data for creating the user.
         db (Session): The database session.
-
     Returns:
         User: The newly created user.
-
     Raises:
         Exception: If an error occurs while fetching the avatar from Gravatar.
     """
@@ -70,8 +60,7 @@ async def create_user(body: UserIn, db: Session) -> User:
 
 async def update_token(user: UserOut, token: str | None, db: Session) -> None:
     """
-     Update the refresh token for a given user in the database.
-
+    Update the refresh token for a given user in the database.
     Args:
         user (User): The user for whom the refresh token will be updated.
         token (str | None): The new refresh token. If None, the token will be removed.
@@ -88,7 +77,6 @@ async def update_token(user: UserOut, token: str | None, db: Session) -> None:
 async def confirm_email(email: str, db: Session) -> None:
     """
     Confirm the email address of the user with the given email  in the database.
-
     Args:
         email (str): The email address of the user to confirm.
         db (Session): The database session.
@@ -98,14 +86,18 @@ async def confirm_email(email: str, db: Session) -> None:
     """
 
     user = await get_user_by_email(email, db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with email:{email} not found",
+        )
     user.confirmed = True
     db.commit()
 
 
 async def update_avatar(email, url: str, db: Session) -> UserOut:
     """
-     Update the avatar URL for the user with the given email address in the database.
-
+    Update the avatar URL for the user with the given email address in the database.
     Args:
         email (str): The email address of the user whose avatar will be updated.
         url (str): The new avatar URL.
@@ -116,7 +108,37 @@ async def update_avatar(email, url: str, db: Session) -> UserOut:
     """
 
     user = await get_user_by_email(email, db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with email:{email} not found",
+        )
     user.avatar = url
     db.commit()
     db.refresh(user)
     return user
+
+
+async def set_user_role(user: UserOut, email: str, role: str, db: Session) -> UserOut:
+    """
+    Set the role of the user with the given email address in the database.
+    Args:
+        user (UserOut): The user who wants to set the role (must be admin).
+        email (str): The email address of the user whose role will be set.
+        role (str): The new role.
+        db (Session): The database session.
+
+    Returns:
+        UserOut: The user with the updated role.
+    """
+    if user.role == "admin":
+        user = await get_user_by_email(email, db)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"User with email:{email} not found",
+            )
+        user.role = role
+        db.commit()
+        db.refresh(user)
+        return user
