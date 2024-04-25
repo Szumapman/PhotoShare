015b40c0b6d4d1e20f56759841be747e61779d7c
OlekKruh <ksalexks@gmail.com>
2024-04-25 23:46:30 +0200

    POST, DELETE, PUT, GET dla zdjencz + routes_testy

--- src/tests/test_photo_routes.py
+++ src/tests/test_photo_routes.py
@@ -11,7 +11,11 @@ class TestPhotoRoutes(unittest.TestCase):
         def test_upload_photo(self, mock_upload_photo):
             photo_data_mock = MagicMock(user_id=1)
             mock_upload_photo.return_value = {'id': 1, 'file_path': '/path/to/photo.jpg'}
-            response = repos_photo.upload_photo(self.db_session_mock, photo_data_mock, user=MagicMock(user_id=1))
+            response = repos_photo.upload_photo(
+                self.db_session_mock,
+                photo_data_mock,
+                user=MagicMock(user_id=1)
+            )
             self.assertEqual(response['id'], 1)
             self.assertEqual(response['file_path'], '/path/to/photo.jpg')
             mock_upload_photo.assert_called_once_with(self.db_session_mock, photo_data_mock, user=MagicMock(user_id=1))
@@ -20,7 +24,12 @@ class TestPhotoRoutes(unittest.TestCase):
         def test_description_photo(self, mock_description_photo):
             mock_description_photo.return_value = {'id': 1, 'description': 'Updated Description'}
             user_mock = MagicMock(user_id=1)
-            response = repos_photo.description_photo(self.db_session_mock, 1, 'Updated Description', user=user_mock)
+            response = repos_photo.description_photo(
+                self.db_session_mock,
+                1,
+                'Updated Description',
+                user=user_mock
+            )
             self.assertEqual(response['id'], 1)
             self.assertEqual(response['description'], 'Updated Description')
             mock_description_photo.assert_called_once_with(self.db_session_mock, 1, 'Updated Description',
@@ -29,7 +38,10 @@ class TestPhotoRoutes(unittest.TestCase):
         @patch('..repository.repos_photo.get_photo_link')
         def test_download_photo(self, mock_get_photo_link):
             mock_get_photo_link.return_value = '/path/to/photo.jpg'
-            response = repos_photo.get_photo_link(self.db_session_mock, 1)
+            response = repos_photo.get_photo_link(
+                self.db_session_mock,
+                1
+            )
             self.assertEqual(response, '/path/to/photo.jpg')
             mock_get_photo_link.assert_called_once_with(self.db_session_mock, 1)
 
@@ -37,7 +49,11 @@ class TestPhotoRoutes(unittest.TestCase):
         def test_delete_photo(self, mock_delete_photo):
             mock_delete_photo.return_value = True
             user_mock = MagicMock(user_id=1)
-            response = repos_photo.delete_photo(self.db_session_mock, 1, user=user_mock)
+            response = repos_photo.delete_photo(
+                self.db_session_mock,
+                1,
+                user=user_mock
+            )
             self.assertTrue(response)
             mock_delete_photo.assert_called_once_with(self.db_session_mock, 1, user=user_mock)
 
