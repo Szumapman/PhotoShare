2a693214ff7e1b8abb64acdce29ab0cf54b183f3
Paweł Szumański <pawel.szumanski@outlook.com>
2024-04-26 13:08:03 +0200

    Create photos.py with upload_photo

---
+++ src/repository/photos.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Session
+
+from src.database.models import Photo
+from src.schemas import PhotoOut
+
+
+async def upload_photo(file_path: str, user_id: int, db: Session):
+    new_photo = Photo(
+        file_path=file_path,
+        description="na razie nie ma",
+        user_id=user_id,
+    )
+    db.add(new_photo)
+    db.commit()
+    db.refresh(new_photo)
+    return PhotoOut(
+        file_path=new_photo.file_path,
+        description=new_photo.description,
+        upload_date=new_photo.upload_date,
+    )
