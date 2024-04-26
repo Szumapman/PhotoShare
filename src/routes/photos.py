acaffd0c5ac376ec168d840d2fc8c6a0035fb79d
Paweł Szumański <pawel.szumanski@outlook.com>
2024-04-26 23:10:56 +0200

    add exception if too many tags

--- src/routes/photos.py
+++ src/routes/photos.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, UploadFile, File, Form
+from fastapi import APIRouter, Depends, UploadFile, File, Form, HTTPException, status
 from sqlalchemy.orm import Session
 import cloudinary
 import cloudinary.uploader
@@ -31,9 +31,14 @@ async def upload_photo(
     current_user: User = Depends(auth_service.get_current_user),
     db: Session = Depends(get_db),
 ) -> PhotoOut:
+    tags = tags[0].split(",")
+    if len(tags) > 5:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Too many tags provided. You can use max of 5 tags.",
+        )
     upload_result = cloudinary.uploader.upload(file.file)
     photo_url = upload_result["url"]
-    tags = tags[0].split(",")
     new_photo = await photos_repository.upload_photo(
         photo_url, current_user.id, description, tags, db
     )
