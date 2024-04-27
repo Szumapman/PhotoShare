4f920b92f3b35e906c579d98b953b2bfaa3ad330
SabinaLimmer <kobylarz.sabina@gmail.com>
2024-04-27 20:36:55 +0200

    Create test_unit_repository_photos.py
    Added test_get_photo_found and test_get_photo_not_found

---
+++ tests/test_unit_repository_photos.py
@@ -0,0 +1,29 @@
+import unittest
+from unittest.mock import MagicMock
+
+from sqlalchemy.orm import Session
+
+from src.database.models import Photo, User
+from src.repository.photos import (
+    get_photo,
+)
+
+class TestPhotos(unittest.IsolatedAsyncioTestCase):
+
+    def setUp(self):
+        self.session = MagicMock(spec=Session)
+        self.user = User(id=1)
+
+    async def test_get_photo_found(self):
+        photo = Photo()
+        self.session.query().filter().first.return_value = photo
+        result = await get_photo(photo_id=1, user=self.user, db=self.session)
+        self.assertEqual(result, photo.file_path)
+
+    async def test_get_photo_not_found(self):
+        self.session.query().filter().first.return_value = None
+        result = await get_photo(photo_id=1, user=self.user, db=self.session)
+        self.assertIsNone(result)
+
+if __name__ == '__main__':
+    unittest.main()
