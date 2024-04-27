904449aaee475e43ad6fb71cabdf8fc2cfafbfa2
pawel544 <140202153+pawel544@users.noreply.github.com>
2024-04-27 15:54:05 +0200

    Add files via upload

--- src/routes/photos.py
+++ src/routes/photos.py
@@ -4,13 +4,15 @@ from fastapi import APIRouter, Depends, UploadFile, File, Form, HTTPException, s
 from sqlalchemy.orm import Session
 import cloudinary
 import cloudinary.uploader
-
+from fastapi.responses import StreamingResponse
 from src.database.db import get_db
 from src.schemas import PhotoOut
 from src.conf.config import settings
 from src.services.auth import auth_service
 from src.database.models import User
 from src.repository import photos as photos_repository
+import qrcode
+from io import BytesIO
 
 
 router = APIRouter(prefix="/photos", tags=["photos"])
@@ -31,8 +33,11 @@ async def upload_photo(
     current_user: User = Depends(auth_service.get_current_user),
     db: Session = Depends(get_db),
 ) -> PhotoOut:
+
     tags = tags[0].split(",")
-    if len(tags) > 5:
+
+    unique_tags= list(set(tags))
+    if len(unique_tags) >= 5:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"Too many tags provided. You can use max of 5 tags.",
@@ -43,3 +48,10 @@ async def upload_photo(
         photo_url, current_user.id, description, tags, db
     )
     return new_photo
+@router.get("/qr/{file_path}")
+async def generation_qr(file_path:str):
+    qr= qrcode.make(file_path)
+    buffer=BytesIO
+    qr.save(buffer, format="PGN")
+    buffer.seek(0)
+    return StreamingResponse(buffer, media_type="image/png")
