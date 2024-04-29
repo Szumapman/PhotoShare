8fef39945d1f29da6da0f34811863ab4af7c5714
Paweł Szumański <pawel.szumanski@outlook.com>
2024-04-30 01:11:26 +0200

    Update test_unit_repository_photos.py

--- tests/test_unit_repository_photos.py
+++ tests/test_unit_repository_photos.py
@@ -1,11 +1,12 @@
 import unittest
+from datetime import datetime
 from unittest.mock import MagicMock
 
 from sqlalchemy.orm import Session
 
-from src.database.models import Photo, User
+from src.schemas import PhotoOut
 from src.repository.photos import (
-    get_photo,
+    get_photo_by_id,
 )
 
 
@@ -13,17 +14,22 @@ class TestPhotos(unittest.IsolatedAsyncioTestCase):
 
     def setUp(self):
         self.session = MagicMock(spec=Session)
-        self.user = User(id=1)
 
     async def test_get_photo_found(self):
-        photo = Photo()
+        photo = PhotoOut(
+            id=1,
+            file_path="test_file_path",
+            description="test",
+            upload_date=datetime.now(),
+        )
         self.session.query().filter().first.return_value = photo
-        result = await get_photo(photo_id=1, db=self.session)
-        self.assertEqual(result, photo.file_path)
+        result = await get_photo_by_id(photo_id=1, db=self.session)
+        print(result.file_path)
+        self.assertEqual(result, photo)
 
     async def test_get_photo_not_found(self):
         self.session.query().filter().first.return_value = None
-        result = await get_photo(photo_id=1, db=self.session)
+        result = await get_photo_by_id(photo_id=1, db=self.session)
         self.assertIsNone(result)
 
 
