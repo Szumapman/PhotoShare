a7dbd3b79cc456d2099bc328603891620e8984b7
Paweł Szumański <pawel.szumanski@outlook.com>
2024-04-29 23:05:00 +0200

    Update admin.py
    add async to index; add  et_active_status instead of deactivate_user; moved set_role from  auth.py

--- src/routes/admin.py
+++ src/routes/admin.py
@@ -1,15 +1,17 @@
 from sqlalchemy.orm import Session
 from src.database.db import get_db
 from src.database.models import User
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, status
 from src.services.auth import auth_service
+from src.repository import users as repository_users
+from src.schemas import UserOut, UserRole
 
 
 router = APIRouter(prefix="/admin", tags=["admin"])
 
 
 @router.get("/")
-def index(
+async def index(
     db: Session = Depends(get_db),
     current_user: User = Depends(auth_service.get_current_user),
 ):
@@ -22,18 +24,44 @@ def index(
     return {"users": users_json}
 
 
-@router.post("/deactivate/{user_id}")
-def deactivate_user(
+@router.post("/activ_status/{user_id}")
+async def set_active_status(
     user_id: int,
-    db: Session = Depends(get_db),
+    new_active_status: bool,
     current_user: User = Depends(auth_service.get_current_user),
-):
+    db: Session = Depends(get_db),
+) -> UserOut:
     if current_user.role != "admin":
         raise HTTPException(status_code=401, detail="Only admins can deactivate users")
-    user = db.query(User).filter(User.id == user_id).first()
-    if user:
-        user.is_active = False
-        db.commit()
-        return {"message": "User deactivated successfully"}
-    else:
-        raise HTTPException(status_code=404, detail="User not found")
+    user = await repository_users.set_user_is_active(user_id, new_active_status, db)
+    return user
+
+
+@router.patch("/set_role/{user_id}")
+async def set_role(
+    user_id: int,
+    new_role: UserRole,
+    current_user: UserOut = Depends(auth_service.get_current_user),
+    db: Session = Depends(get_db),
+) -> UserOut:
+    """
+    Updates the user's role
+
+    Args:
+        user_id (int): The id of the user to update
+        new_role (UserRole): The new user role
+        current_user (User): The current user
+        db (Session): Database session
+
+    Returns:
+        UserOut: The updated user
+
+    Raises:
+        HTTPException: If the current user is not admin.
+    """
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Only admin users are allowed to set the role of users",
+        )
+    return await repository_users.set_user_role(user_id, new_role.role, db)
