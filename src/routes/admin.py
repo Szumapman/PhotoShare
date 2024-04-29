06df462d4744273ec47367c46477ffc67b179a08
BettyBeetle <143384409+BettyBeetle@users.noreply.github.com>
2024-04-29 11:10:59 +0200

    user_authorization

--- src/routes/admin.py
+++ src/routes/admin.py
@@ -2,13 +2,19 @@ from sqlalchemy.orm import Session
 from src.database.db import get_db
 from src.database.models import User
 from fastapi import APIRouter, HTTPException, Depends
+from src.services.auth import auth_service
 
 
 router = APIRouter(prefix="/admin", tags=["admin"])
 
 
 @router.get("/")
-def index(db: Session = Depends(get_db)):
+def index(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(auth_service.get_current_user),
+):
+    if current_user.role != "admin":
+        raise HTTPException(status_code=403, detail="Unauthorized")
     users = db.query(
         User.id, User.username, User.email, User.created_at, User.is_active
     ).all()
@@ -17,7 +23,13 @@ def index(db: Session = Depends(get_db)):
 
 
 @router.post("/deactivate/{user_id}")
-def deactivate_user(user_id: int, db: Session = Depends(get_db)):
+def deactivate_user(
+    user_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(auth_service.get_current_user),
+):
+    if current_user.role != "admin":
+        raise HTTPException(status_code=401, detail="Only admins can deactivate users")
     user = db.query(User).filter(User.id == user_id).first()
     if user:
         user.is_active = False
