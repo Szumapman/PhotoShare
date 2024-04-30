21126ad7c275c403b6e89e6c9b7a727926f8c8c8
Paweł Szumański <pawel.szumanski@outlook.com>
2024-04-30 11:26:48 +0200

    deleta unnessesery field user_id from tags

--- src/database/models.py
+++ src/database/models.py
@@ -109,7 +109,6 @@ class Tag(Base):
     __tablename__ = "tags"
     id = Column(Integer, primary_key=True)
     tag_name = Column(String(50), nullable=False)
-    user_id = Column(Integer, ForeignKey("users.id"))
 
     photos = relationship("Photo", secondary="photo_tags", back_populates="tags")
 
