cc91bf9511515f1e896e338acb9ad101094e45a0
Paweł Szumański <pawel.szumanski@outlook.com>
2024-04-30 13:44:42 +0200

    Update test_unit_repository_photos.py

--- tests/test_unit_repository_photos.py
+++ tests/test_unit_repository_photos.py
@@ -2,28 +2,62 @@ import unittest
 from unittest.mock import MagicMock
 
 from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
 
 from src.database.models import Photo, User
+from src.schemas import UserOut
 from src.repository.photos import (
     delete_photo,
 )
 
-class TestPhotos(unittest.IsolatedAsyncioTestCase):
 
+class TestPhotos(unittest.IsolatedAsyncioTestCase):
     def setUp(self):
         self.session = MagicMock(spec=Session)
-        self.user = User(id=1)
+        self.user = UserOut(
+            id=1,
+            username="user",
+            email="test@email.com",
+            password="testPass1",
+            role="standard",
+        )
 
     async def test_delete_photo_found(self):
-        photo = Photo()
+        photo = Photo(id=1, user_id=1)
         self.session.query().filter().first.return_value = photo
         result = await delete_photo(photo_id=1, user=self.user, db=self.session)
         self.assertEqual(result, photo)
 
+    async def test_delete_photo_as_admin(self):
+        photo = Photo(id=1, user_id=1)
+        self.session.query().filter().first.return_value = photo
+        user = UserOut(
+            id=2,
+            username="user",
+            email="test@email.com",
+            password="testPass1",
+            role="admin",
+        )
+        result = await delete_photo(photo_id=1, user=user, db=self.session)
+        self.assertEqual(result, photo)
+
     async def test_delete_photo_not_found(self):
         self.session.query().filter().first.return_value = None
-        result = await delete_photo(photo_id=1, user=self.user, db=self.session)
-        self.assertIsNone(result)
+        with self.assertRaises(HTTPException) as context:
+            await delete_photo(photo_id=1, user=self.user, db=self.session)
+        self.assertEqual(context.exception.status_code, 404)
+        self.assertEqual(context.exception.detail, "Photo not found")
 
-if __name__ == '__main__':
+    async def test_delete_photo_unauthorized(self):
+        photo = Photo(id=1, user_id=2)
+        self.session.query().filter().first.return_value = photo
+        with self.assertRaises(HTTPException) as context:
+            await delete_photo(photo_id=1, user=self.user, db=self.session)
+        self.assertEqual(context.exception.status_code, 403)
+        self.assertEqual(
+            context.exception.detail, "Only owner of the photo or admin can delete it."
+        )
+
+
+if __name__ == "__main__":
     unittest.main()
