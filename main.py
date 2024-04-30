8630be48493902cdd02d454252c53ddb403a5e7e
Paweł Szumański <pawel.szumanski@outlook.com>
2024-04-30 20:05:09 +0200

    add  users.router

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi_limiter import FastAPILimiter
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.conf.config import settings
-from src.routes import auth, comments, admin
+from src.routes import auth, comments, admin, users
 from src.routes import photos
 import uvicorn
 from dotenv import load_dotenv
@@ -26,6 +26,7 @@ app.include_router(auth.router, prefix="/api")
 app.include_router(photos.router, prefix="/api")
 app.include_router(admin.router, prefix="/api")
 app.include_router(comments.router, prefix="/api")
+app.include_router(users.router, prefix="/api")
 
 
 async def startup_event():
