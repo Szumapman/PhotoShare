33383b44a18d07d2fd4fcb2f13b920b478889c09
Paweł Szumański <pawel.szumanski@outlook.com>
2024-04-30 21:13:39 +0200

    add get_current_user

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.functions import current_user
 
-from src.database.models import User
 from src.repository import users as users_repository
 from src.database.db import get_db
-from src.schemas import UserPublicProfile
+from src.schemas import UserPublicProfile, UserOut
 from src.services.auth import auth_service
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -14,7 +14,7 @@ router = APIRouter(prefix="/users", tags=["users"])
 async def get_user_profile(
     user_id: int,
     db: Session = Depends(get_db),
-    current_user: User = Depends(auth_service.get_current_user),
+    current_user: UserOut = Depends(auth_service.get_current_user),
 ):
     """
     Retrieve a user's profile based on their email address.
@@ -22,9 +22,25 @@ async def get_user_profile(
     Args:
         user_id (int): The id of the user whose profile is to be retrieved.
         db (Session): The database session.
-        current_user (User): The current authenticated user.
+        current_user (UserOut): The current authenticated user.
 
     Returns:
         UserProfile: The user's profile information if found.
     """
     return await users_repository.get_user_public_profile(user_id, db)
+
+
+@router.get("/me/", response_model=UserOut)
+async def get_current_user(
+    current_user: UserOut = Depends(auth_service.get_current_user),
+):
+    """
+    Retrieve an authenticated user profile.
+
+    Args:
+         current_user (UserOut): The current authenticated user.
+
+    Returns:
+        UserOut: The user's profile information if found.
+    """
+    return current_user
