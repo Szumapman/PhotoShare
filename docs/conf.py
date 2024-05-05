787dbe17b5e92f5499c63b59d3e2e894033dea7a
Paweł Szumański <pawel.szumanski@outlook.com>
2024-05-05 15:12:12 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,24 +9,24 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.abspath(".."))
 
-project = 'PhotoShare'
-copyright = '2024, Beata Chrząszcz, Oleksy Kruh, Paweł Szumański, Sabina Limmer'
-author = 'Beata Chrząszcz, Oleksy Kruh, Paweł Szumański, Sabina Limmer'
-release = '1.0'
+project = "PhotoShare"
+copyright = "2024, Beata Chrząszcz, Oleksy Kruh, Paweł Szumański, Sabina Limmer"
+author = "Beata Chrząszcz, Oleksy Kruh, Paweł Szumański, Sabina Limmer"
+release = "1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'nature'
-html_static_path = ['_static']
-
+html_theme = "nature"
+html_theme_options = {"sidebarwidth": 400}
+html_static_path = ["_static"]
