0262a674fde64189fd15801d8e23c8df1c4f8c65
Paweł Szumański <pawel.szumanski@outlook.com>
2024-05-05 13:15:52 +0200

    add PHOTO_SEARCH_ENUMS

--- src/conf/constant.py
+++ src/conf/constant.py
@@ -2,3 +2,4 @@ MAX_DESCRIPTION_LENGTH = 1000
 MAX_TAG_NAME_LENGTH = 50
 MAX_COMMENT_LENGTH = 1000
 MAX_USERNAME_LENGTH = 255
+PHOTO_SEARCH_ENUMS = ["upload_date-desc", "upload_date-asc"]  # add rating when ready
