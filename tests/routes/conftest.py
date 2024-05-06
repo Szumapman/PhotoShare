b31788f6bac308d7b72c6c9a3abd35e35d4b6514
Paweł Szumański <pawel.szumanski@outlook.com>
2024-05-06 09:12:05 +0200

    add photo fixtures and function

--- tests/routes/conftest.py
+++ tests/routes/conftest.py
@@ -1,11 +1,15 @@
+from io import BytesIO
+
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from PIL import Image
+
 
 from main import app
 from src.services.auth import auth_service
-from src.database.models import Base, User
+from src.database.models import Base, User, Photo
 from src.database.db import get_db
 
 
@@ -95,6 +99,65 @@ def user():
     return User(username="userTest", email="test@email.com", password=TEST_PASSWORD)
 
 
+@pytest.fixture(scope="function")
+def user_for_token():
+    return {
+        "username": "user_token",
+        "email": "user_token@example.com",
+        "password": TEST_PASSWORD,
+    }
+
+
+@pytest.fixture(scope="module")
+def mock_image(width=500, height=500, color=(255, 0, 0)):
+    """
+    Create a mock picture.
+
+    Args:
+    - width (int): Width of the picture.
+    - height (int): Height of the picture.
+    - color (tuple): RGB color tuple for the picture background.
+
+    Returns:
+    - BytesIO: BytesIO object containing the mock picture.
+    """
+
+    image = Image.new("RGB", (width, height), color)
+    image_bytes_io = BytesIO()
+    image.save(image_bytes_io, format="PNG")
+    image_bytes_io.seek(0)
+
+    return image_bytes_io
+
+
+@pytest.fixture
+def photo():
+    class PhotoTest:
+        def __init__(self, id, file_path, qr_path, description, user_id):
+            self.id = id
+            self.file_path = file_path
+            self.qr_path = qr_path
+            self.description = description
+            self.user_id = user_id
+
+        def dict(self):
+            return {
+                "id": self.id,
+                "file_path": self.file_path,
+                "qr_path": self.qr_path,
+                "description": self.description,
+                "user_id": self.user_id,
+            }
+
+    return PhotoTest(
+        id=1,
+        file_path="photo_url",
+        qr_path="qr_path",
+        description="description",
+        user_id=1,
+    )
+
+
 def add_user_to_db(user: user, db: session):
     new_user = User(
         username=user.username,
@@ -118,12 +181,24 @@ def add_not_confirmed_user_to_db(user: user, db: session):
     return new_user
 
 
-# async def add_user_to_db_with_tokens(user: user, db: session):
-#     new_user = add_user_to_db(user=user, db=db)
-#     access_token = await auth_service.create_access_token(data={"sub": new_user.email})
-#     new_user.refresh_token = await auth_service.create_refresh_token(
-#         data={"sub": new_user.email}
-#     )
-#     db.commit()
-#     db.refresh(new_user)
-#     return new_user
+def add_user_with_token(user: user, db: session):
+    new_user = add_user_to_db(user=user, db=db)
+    new_user.refresh_token = "<REFRESH TOKEN>"
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+def create_x_photos(no_of_photos: int, db: session):
+    photos = []
+    for i in range(no_of_photos):
+        photo = Photo(
+            file_path="photo_url",
+            qr_path="qr_path",
+            description="description",
+            user_id=1,
+        )
+        db.add(photo)
+        db.commit()
+        photos.append(photo)
+    return photos
